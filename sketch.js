90127b2848340f8c6981c817d4e5574cd4e15127
chaminjoo <k22tmfrl@naver.com>
2014-09-11 15:23:30 +0900

    sketch.js

--- sketch.js
+++ sketch.js
@@ -5,14 +5,23 @@ function setup() {
 
   stroke(255, 0, 0);
 
+/*
   var x;
 
-  x = 100;
-  ellipse(x, 100, 50, 50);
+  x = 1;
+  ellipse(x * 100, 100, 50, 50);
 
-  x = 250;
-  ellipse(x, 100, 50, 50);
+  x = 2;
+  ellipse(x * 100, 100, 50, 50);
 
-  x = 300;
-  ellipse(x, 100, 50, 50);
+  x = 3;
+  ellipse(x * 100, 100, 50, 50);
+
+*/
+for (var x = 1; x <=3; x++ )
+{
+ellipse(x * 100, 100, 50, 50);
 }
+
+}
+
